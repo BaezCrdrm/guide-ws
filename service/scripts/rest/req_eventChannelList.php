fa52c7238430760b50854049d2f49628c2b97ff6
BaezCrdrm <dev.baez_crdrm@outlook.com>
2018-06-07 12:45:36 -0500

    Cambios API. Regresa URL de canal de evento.

--- service/scripts/rest/req_eventChannelList.php
+++ service/scripts/rest/req_eventChannelList.php
@@ -1,9 +1,9 @@
 <?php
 $id = $_REQUEST["evId"];
 require "../queries.php";
-$query = "SELECT channels.ch_id, channels.ch_name, channels.ch_abv, channels.ch_img ";
-$query .= "FROM channels INNER JOIN event_channel ON event_channel.ch_id = channels.ch_id ";
-$query .= "WHERE event_channel.ev_id = '$id'";
+$query = "SELECT c.ch_id, c.ch_name, c.ch_abv, c.ch_img, ec.ev_ch_url ";
+$query .= "FROM channels c INNER JOIN event_channel ec ON ec.ch_id = c.ch_id ";
+$query .= "WHERE ec.ev_id = '$id'";
 
 header("Access-Control-Allow-Origin: *");
 echo request($query);
