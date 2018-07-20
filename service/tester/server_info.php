6410756515573d70e6f7d45477cdfb8e9c810abe
BaezCrdrm <dev.baez_crdrm@outlook.com>
2018-07-20 18:59:37 -0500

    Filter query

--- service/tester/server_info.php
+++ service/tester/server_info.php
@@ -1,14 +1,10 @@
 <?php
-error_reporting(0);
-ini_set('display_errors', 0);
-$id = $_REQUEST["evId"];
-$tp = $_REQUEST["tpId"];
-require "../queries.php";
+require "../scripts/queries.php";
 
-$query = "SELECT event.ev_id, event.ev_name, event.ev_sch, event.ev_sch_end, event.ev_des";
+$query = "SELECT event.ev_id, event.ev_name, event.ev_sch, ev_sch_end, event.ev_des";
 $query .= ", types.tp_id, types.tp_name FROM event INNER JOIN type_event ";
 $query .= "ON type_event.ev_id = event.ev_id INNER JOIN types ON type_event.tp_id = types.tp_id ";
-$query .= "WHERE event.ev_sch_end < NOW()";
+$query .= "WHERE ev_sch_end < NOW()";
 
 if($id != null && $id != "null")
 {
@@ -20,6 +16,13 @@ elseif($tp != null && $tp != "null") {
 
 $query .= " ORDER BY event.ev_sch ASC";
 
+echo $query;
+
 header("Access-Control-Allow-Origin: *");
-echo request($query);
+// echo request($query);
+
+echo "<br>";
+echo request("SELECT NOW()");
+
+
 ?>
