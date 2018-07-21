1484bd2f62dc3a15138b9131f43bc5b8ebded655
BaezCrdrm <dev.baez_crdrm@outlook.com>
2018-07-20 20:10:52 -0500

    Correct where statement

--- service/scripts/rest/req_events.php
+++ service/scripts/rest/req_events.php
@@ -8,14 +8,13 @@ require "../queries.php";
 $query = "SELECT event.ev_id, event.ev_name, event.ev_sch, event.ev_sch_end, event.ev_des";
 $query .= ", types.tp_id, types.tp_name FROM event INNER JOIN type_event ";
 $query .= "ON type_event.ev_id = event.ev_id INNER JOIN types ON type_event.tp_id = types.tp_id ";
-$query .= "WHERE event.ev_sch_end >= ADDTIME('NOW()','05:00:00')";
 
 if($id != null && $id != "null")
 {
-	$query .= " AND event.ev_id = '$id'";
-	// $query .= "WHERE AND event.ev_id = '$id'";
+	$query .= " WHERE event.ev_id = '$id'";
 }
 elseif($tp != null && $tp != "null") {
+	$query .= "WHERE event.ev_sch_end >= ADDTIME('NOW()','05:00:00')";
 	$query .= " AND type_event.tp_id = $tp";
 	// $query .= "WHERE AND type_event.tp_id = $tp";
 }
